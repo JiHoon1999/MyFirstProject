af5ed8a904fe2aa2181e85053c1892c1271850ee
ThanhBui <buithanhthanh199@gmail.com>
2022-04-04 21:43:49 +0700

    chuong triinh co ban

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int nam;
-	cout << "Nhap 1 nam bat ki :" << endl;
-	cin >> nam;
+int main() {
 
-	if (nam >= 0 && nam <= 3000) {
-		if (nam % 4 == 0) {
-			cout << "Nam nhuan" << endl;
-		}
-		else {
-			cout << "Nam thuong" << endl;
 
-		}
-	}
-
+	cout << endl;
+	system("pause");
 	return 0;
 }
