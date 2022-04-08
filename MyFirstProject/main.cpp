75f19cc7966a1dc9709792a82a9d5d488f28217e
ThanhBui <buithanhthanh199@gmail.com>
2022-04-08 21:38:14 +0700

    SO CHAN LE 100 DEN 0

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
 		
-	for (int i =0 ; i <=100 ; i=i+2) {
+	for (int i =100 ; i >=0 ; i=i-2) {
 		cout << i << endl;
 	}
-	for (int i = 1; i <= 100; i = i + 2) {
+	for (int i = 99; i >= 1; i = i - 2) {
 		cout << i << endl;
 	}
 	cout << endl;
