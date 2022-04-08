c33e31b89eb90bf9cc676c7079e70bc81fb9f500
ThanhBui <buithanhthanh199@gmail.com>
2022-04-08 21:32:40 +0700

    dem so chan le

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main() {
 		
-	for (int i =100 ; i >= 0; i--) {
+	for (int i =0 ; i <=100 ; i=i+2) {
 		cout << i << endl;
 	}
-	
+	for (int i = 1; i <= 100; i = i + 2) {
+		cout << i << endl;
+	}
 	cout << endl;
 	system("pause");
 	return 0;
