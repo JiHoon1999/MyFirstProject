bb9ac8b968afbd099daa888164324c1103532412
ThanhBui <buithanhthanh199@gmail.com>
2022-04-08 22:08:48 +0700

    Bai tap 1 8/4

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int main() {
+	int n;
+	cout << "Vui long nhap vao n:";
+	cin >> n;
+	if (n > 0) {
+		for (int i = 0; i < n; i++) {
+			if ( i % 2 != 0) {
+				cout << i << " ";
+			}
+		}
 		
-	for (int i =0 ; i <=30 ; i++) {
-		if(i%3==0)
-		cout << i << endl;
 	}
-	
+	else {
+		cout << "Ban da nhap sai" << endl;
+	}
 	cout << endl;
 	system("pause");
 	return 0;
