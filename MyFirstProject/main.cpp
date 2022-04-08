6bfdd34c32afeb4dc77ede0aafba8a1d886e4d35
ThanhBui <buithanhthanh199@gmail.com>
2022-04-08 21:40:46 +0700

    Tat ca chia het cho 3

--- MyFirstProject/main.cpp
+++ MyFirstProject/main.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main() {
 		
-	for (int i =100 ; i >=0 ; i=i-2) {
+	for (int i =0 ; i <=30 ; i++) {
+		if(i%3==0)
 		cout << i << endl;
 	}
-	for (int i = 99; i >= 1; i = i - 2) {
-		cout << i << endl;
-	}
+	
 	cout << endl;
 	system("pause");
 	return 0;
